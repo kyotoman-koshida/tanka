05dca699f77d42a093808c5a313e3282968e102e
Tatsuo Koshida <tkoshida@skywill.jp>
2024-07-09 22:42:25 +0900

    フロント側の画面の移り変わりについての改修

--- app.py
+++ app.py
@@ -21,25 +21,33 @@ st.write('選択した詠み手:', model_name)
 #メインフォームの設定
 with st.form(key = "Letter Form", clear_on_submit = False):
     st.title("情景 入力欄")
-    body = st.empty()
     if ("letter_body" in st.session_state.keys()): 
-        ret = body.text_area(
+        ret = st.text_input(
             label = "短歌に詠み込みたい情景を入力してください。",
+            value = st.session_state["scene_text"]
+        )
+        text_area = st.text_area(
+            label = "一首詠みました。",
             value = st.session_state["letter_body"]
         )
+        sub = st.form_submit_button("もう一首詠む")
     else:
-        ret = body.text_area(
+        ret = st.text_input(
             label = "短歌に詠み込みたい情景を入力してください。",
             value = ""
         )
-    sub = st.form_submit_button("一首詠む")
+        sub = st.form_submit_button("一首詠む")
 
-# TODO: 一首詠んだ後のボタンを、「もう一度詠む」ボタンに変更させる
+if ("letter_body" in st.session_state.keys()):
+    if st.button("最初から"):
+        del st.session_state["letter_body"]
+        st.experimental_rerun()
 
 if sub:
     tanka_generater = TankaGenerater(tanka_config)
     #LLMによる推論を実行。
     with st.spinner(text = "作歌アンド推敲中..."):
         text  = tanka_generater.generate_tanka(scene_text=ret, model_name=model_name)
+        st.session_state["scene_text"] = ret
         st.session_state["letter_body"] = text
         st.experimental_rerun()
