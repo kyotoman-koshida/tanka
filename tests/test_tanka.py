306a494691325ebee2d57f2660ddb0125d2f0e4e
Tatsuo Koshida <tkoshida@skywill.jp>
2024-07-19 23:01:20 +0900

    テストケースの追加

--- tests/test_tanka.py
+++ tests/test_tanka.py
@@ -3,22 +3,36 @@
 """Tests for `tanka` package."""
 
 import pytest
-
-
-from tanka import tanka
+from pathlib import Path
+from src.tanka.config.config import TankaConfig
+from src.tanka import tanka
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def tanka_config(monkeypatch):
+    """TankaGeneratorのインスタンス化で必要な設定クラスを用意する。"""
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    # NOTE:TankaConfigは内部でLLMモデルの格納しているパスを参照しているためファイルのパスを合わせ流。
+    test_dir = Path(__file__).parent.parent
+    monkeypatch.chdir(test_dir)
 
+    tanka_config = TankaConfig()
+    return tanka_config
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_TankaGenerater_chechtype_1(tanka_config):
+    """Phi3を使ってTankaGeneraterで短歌を生成した時に、生成した出力がstr型になっているか確認"""
+
+    tanka_generater = tanka.TankaGenerater(tanka_config)
+    scene = "夏より冬の方が好きだ"
+    model_name = "Phi-3"
+    generated_text = tanka_generater.generate_tanka(scene_text=scene, model_name=model_name)
+    assert type(generated_text) == str
+
+def test_TankaGenerater_chechtype_2(tanka_config):
+    """ELYZAを使ってTankaGeneraterで短歌を生成した時に、生成した出力がstr型になっているか確認"""
+
+    tanka_generater = tanka.TankaGenerater(tanka_config)
+    scene = "夏より冬の方が好きだ"
+    model_name = "ELYZA"
+    generated_text = tanka_generater.generate_tanka(scene_text=scene, model_name=model_name)
+    assert type(generated_text) == str
